f5260e698b607c47b97e583403513a2784936128
Pratik Shinde <pratikshinde1497@gmail.com>
2020-11-03 13:32:00 +0530

    Auto-generated commit

---
+++ assets/color.ts
@@ -0,0 +1,19 @@
+export namespace COLORS {
+  export const PRIMARY_COLOR = '#20A4B6';
+  export const PRIMARY_DARK = '#051E23';
+  export const CARD = {
+    1: '#961111',
+    2: '#B320B6',
+    3: '#FF9900',
+    4: '#EA0F0F',
+    5: '#7472D2',
+  }
+  export const WHITE = '#FFFFFF';
+  export const DEFAULT_BACKGROUND = '#F1F0F0';
+  export const LIGHT_BLUE_BACK = '#D9E3E3';
+  export const INPUT_BACK = '#E8E8E8';
+  export const HEAD_LIGHT = '#C4C4C4';
+  export const HEAD_DARK = '#979292';
+}
+
+// console.log(COLORS.);
